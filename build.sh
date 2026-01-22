8656507961cb2c581c7a811492a724d58d825b0a
prz <prz@zeta2.ch>
2026-01-22 10:06:20 +0100

    -13 disttopo

--- build.sh
+++ build.sh
@@ -36,9 +36,10 @@ D1=draft-lsr-distopflood
 D2=draft-rigatoni-lsr-isis-fragment-timestamping
 D3=draft-ietf-lsr-flood-reduction-arch
 
-for D in $D2 # draft-rigatoni-lsr-isis-fragment-timestamping draft-lsr-distopflood
+for D in $D2 $D1
 do
-  xml2rfc --allow-local-file-access --text --html $D.xml
+  xml2rfc --allow-local-file-access --text $D.xml
+  xml2rfc --allow-local-file-access --html $D.xml
   xml2rfc --allow-local-file-access --expand $D.xml
   # echo 'sed\ing stroke none'
   # sed -i 's/stroke="none"//g' $D.exp.xml
